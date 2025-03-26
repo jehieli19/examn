c9f3ebcd34ef2075b1d94d024366f50aacccbd2d
jehieli19 <jehielixool@gmail.com>
2025-03-25 18:15:56 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿Using System; 
+Using Syste. collectiond. generic; 
+Using Syste.linq;
+Using Syste.Text.Json; 
+
+
+public class Product;
+{
+ public int buscar Product { get, set;};
+ public int  codigo { get, set;};
+ public string nombre_Producto  { get, set;};
+ public sting CantidadStock { get, set }; 
+public int Precio_Producto { get, set }; 
+public int CantidadStocket { get, set }; 
+}
+
+public class Compra 
+{ 
+private List <  Product > Productos = new List <Productos> ();
+private List <  codigo >  codigo  = new List   <codigo> ();
+private List <  nombre_Producto >  nombre_Productos  = new List   <nombre_Producto> ();
+private List <  CantidadStock >  CantidadStock  = new List   <CantidadStock> ();
+private List <  Precio_Producto >  Precio_Producto  = new List   <Precio_Producto> ();
+}
+
+pulic class invetario 
+{
+private List <  Product > invetario
+private sting inventarioJso  = " inventaruio.Json" ;
+}
+{       
+pulic invetario; 
+if ( file.existen(inventarioJson))
+}
+else 
+{
+    if (file.no existen(inventarioJso))
+}
+{
+  Console.WriteLine("Quieres buscar productos?{codigo }")
+Console.writeline("quieres cancelar el producto?")
+ Console.writeline("Escribe el codio del producto ")
+if (" si dices si ")  
+
+}
+
+public class Pagar;
+{
+console.WriteLine ( pagar +productos +codigo + nombre_Producto +canridadStock + Precio_Producto) 
+console.WriteLine( $ "nombre_producto, codigo ")
+}
+ public string pagar = pagar ( pagar +productos +codigo + nombre_Producto +canridadStock + Precio_Producto)
+ producto.add (new producto) " COCA",$ 15,
+  producto.add (new producto) GALLETA ,$ 20 , 
+  producto.add (new producto) manzana, $ 7 ;
+ codigo.add ( codigo ) "COCA = 1012 , GALLETA = 1910 , manzana = 1234;"
